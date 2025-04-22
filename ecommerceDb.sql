fc6b529d7b3ff3eecbd2188d1dbb0fe9011ae922
Denis Mwanzia <denismwanzia56@gmail.com>
2025-04-22 21:24:31 +0300

    Added SQL script to create product table.

--- ecommerceDb.sql
+++ ecommerceDb.sql
@@ -21,4 +21,16 @@ CREATE TABLE productCategory (
     categoryName VARCHAR(100) NOT NULL
 );
 
+-- Table: product
+CREATE TABLE product (
+    productID INT AUTO_INCREMENT PRIMARY KEY,
+    productName VARCHAR(150) NOT NULL,
+    brandID INT,
+    categoryID INT,
+    basePrice DECIMAL(10, 2),
+    description TEXT,
+    FOREIGN KEY (brandID) REFERENCES brand(brandID),
+    FOREIGN KEY (categoryID) REFERENCES productCategory(categoryID)
+);
+
 
