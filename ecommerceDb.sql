f5b50ff32fd96f768f35463221f61acd75169818
Denis Mwanzia <denismwanzia56@gmail.com>
2025-04-22 21:20:28 +0300

    Added SQL script to create E-commerce database and script to use the created database.

---
+++ ecommerceDb.sql
@@ -0,0 +1,12 @@
+
+-- ==============================================
+-- E-commerce Database Schema
+-- ==============================================
+
+-- Create E-commerce Database
+CREATE DATABASE ecommercedb;
+
+-- Use E-commerce Database
+USE ecommercedb;
+
+
